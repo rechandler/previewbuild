8307036fd6e383b5ab3994ad26be4e98066de57b
Ryann Chandler <44813036+rechandler@users.noreply.github.com>
2025-05-26 20:17:58 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const prNumber = process.env.PR_NUMBER;
-let config;
-if (!prNumber) {
-  config = defineConfig({
-    plugins: [react()],
-  });
-} else {
-  config = defineConfig({
-    plugins: [react()],
-    base: `/previewbuild-pr-${prNumber}/`,
-  });
-}
-
-export default config;
+export default defineConfig({
+  plugins: [react()],
+});
